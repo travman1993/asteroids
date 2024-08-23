a4c4cec5fa7579f61233afdc10c7094154d33680
Travis Rodriguez <travisgolembiewski@gmail.com>
2024-08-22 22:47:02 -0400

    adding player and circles

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def main():
     pygame.init()
     # display window size
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # Initialize player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     # FPS starter
     clock = pygame.time.Clock()
     # dt variable
@@ -21,6 +21,8 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill((0, 0, 0))
+        # call player to loop
+        player.draw(screen)
         pygame.display.flip()
         # FPS to 60
         return dt == clock.tick(60) / 1000
